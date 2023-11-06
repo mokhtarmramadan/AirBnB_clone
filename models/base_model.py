611edd4e3c8cdf253f0fba04652086c10baedb34
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-11-06 16:04:16 +0200

    Updated base_model.py to version 1

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,23 @@ BaseModel that defines all common attributes/methods for other classes
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     "A parent class that all the upcoming classes will inherit form"
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        "Initiate a attributes"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if len(kwargs) != 0:
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, time_format)
+                else:
+                    self.__dict__[k] = v
 
     def save(self):
         "Updates the public instance attribute updated_at with"
@@ -20,12 +31,13 @@ class BaseModel:
     def to_dict(self):
         "Returns a dictionary representation of the class"
         dict_objct = self.__dict__.copy()
+        dict_objct['id'] = self.id
+        dict_objct['created_at'] = str(self.created_at)
+        dict_objct['updated_at'] = str(self.updated_at)
         dict_objct['__class__'] = self.__class__.__name__
-        dict_objct['created_at'] = self.created_at.isoformat()
-        dict_objct['updated_at'] = self.updated_at.isoformat()
-        return dict_objct
-    
-        
+        return (dict_objct)
+
     def __str__(self):
-        "should print: [<class name>] (<self.id>) <self.__dict__>"
-        return "[{}] ({}) <{}>".format(self.__class__.__name__, self.id, self.to_dict())
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)

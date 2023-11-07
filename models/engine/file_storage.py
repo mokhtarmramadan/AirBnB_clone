748ca82fefd2d6c03ad1f0a15caf6522beecda11
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-11-07 14:34:01 +0200

    Fixed the engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""This model supplies one class,
+FileStorage"""
 import json
 from models.base_model import BaseModel
-"""
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-"""
 
 
 class FileStorage:
-    """Represent an abstracted storage engine.
-
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-    """
+    "Represent an abstracted storage engine"
     __file_path = "file.json"
     __objects = {}
 
@@ -32,20 +20,20 @@ class FileStorage:
         FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
+        """Serialize __objects to the JSON file"""
         odict = FileStorage.__objects
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(objdict, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """Deserialize the JSON file"""
         try:
             with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+                dict_objct = json.load(f)
+                for v in dict_objct.values():
+                    class_name = v["__class__"]
+                    del v["__class__"]
+                    self.new(eval(class_name)(**v))
         except FileNotFoundError:
             return
